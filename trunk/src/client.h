4eb10bd01a8a3760c8ead254e90502382d009dae
eblis <eblis@b4c5eeb3-f702-0410-9c1d-9c571de72459>
2005-10-23 20:03:40 +0000

    KDevelop acting up again ...

--- trunk/src/client.h
+++ trunk/src/client.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2005 by Cristi, Ion, Diana                              *
+ *   Copyright (C) 2005 by Cristi, Diana, Ion                              *
  *                                                                         *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
