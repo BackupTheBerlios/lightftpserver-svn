1b120b7819944a4ea1837866c5604355456cae0b
comp_ <comp_@b4c5eeb3-f702-0410-9c1d-9c571de72459>
2005-10-26 21:56:39 +0000

    Daemonized

--- trunk/src/ftp_server.cpp
+++ trunk/src/ftp_server.cpp
@@ -33,7 +33,13 @@ This is the main server file.
 
 #include "configUtils.h"
 #include "logClient.h"
+#include <signal.h>
+#include <syslog.h>
 #include <time.h>
+
+#include <sys/stat.h>
+
+#define MAXFD 64
 
 /**
 Current verbosity level ...
@@ -45,10 +51,82 @@ int Verbosity = 5;
 
 int main()
 {
+  int rcode, i, listenfd, connfd, len;
+  struct sockaddr_in addr;
+  
+  //daemonization
+  rcode = fork();
+  if (rcode == -1) {
+    syslog(LOG_FTP|LOG_ERR, "daemonization stage 1 %m");
+    exit(1);
+  }
+  else if (rcode == 0)
+    //parent
+    exit(0);
+
+  //child 1
+  setsid();
+  signal(SIGHUP, SIG_IGN);
+
+  if (rcode == -1) {
+    syslog(LOG_FTP|LOG_ERR, "daemonization stage 2 %m");
+    exit(1);
+  }
+  else if (rcode == 0)
+    //parent
+    exit(0);
+
+  //child 2
+  chdir("/");
+  umask(0);
+
+  for (i = 0; i < MAXFD; i++)
+    close(i);
+
+  //daemon
+  if ( (listenfd = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
+    syslog(LOG_FTP|LOG_ERR, "socket: %m");
+    exit(1);
+  }
+
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(21);
+  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+ 
+  if ( (rcode = bind(listenfd, (struct sockaddr *)&addr, sizeof(addr))) == -1) {
+    syslog(LOG_FTP|LOG_ERR, "bind: %m");
+    exit(1);
+  }
+
+  if ( (rcode = listen(listenfd, 5)) == -1) {
+    syslog(LOG_FTP|LOG_ERR, "listen: %m");
+    exit(1);
+  }
+
+  syslog(LOG_FTP|LOG_INFO, "FTP Server started. Waiting for connections...");
+  while (1) {
+    if ( (connfd = accept(listenfd,(struct sockaddr *) &addr, (socklen_t *)&len)) == -1) {
+      syslog(LOG_FTP|LOG_ERR, "accept: %m");
+      exit(1);
+    }
+    
+    rcode = fork();
+    if (rcode == -1) {
+      syslog(LOG_FTP|LOG_ERR, "child: %m");
+      exit(1);
+    } else if (rcode != 0) {
+      //child
+      close(listenfd);
+      //start the actual server :D
+    }
+    //parent
+    close(connfd);
+  }
+
 				CLogClient log(5);
         srand(time(NULL));
-        int i = random() % 1000;
-        printf("FTP Server started ...\nWaiting for clients to server ...\n");
+        i = random() % 1000;
+//         printf("FTP Server started ...\nWaiting for clients to server ...\n");
 //        Log(0, "This is a log entry with a string (%s) and a number(%d)", "this is the string", i);
         char buffer[INI_LINE_SIZE];
         long caca;
